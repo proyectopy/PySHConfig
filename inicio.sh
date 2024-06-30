6f871a5a8db0a427563e57064e799bc945eae700
Wild <118611832+proyectopy@users.noreply.github.com>
2024-06-30 13:30:10 +0200

    Correccion de errores en inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -13,10 +13,10 @@
 # SCRIPS NECESARIOS
 ########################################################################
 . imports/scripts_imp.sh
-. inport/info_imp.sh
-. import/sist_imp.sh
-. import/serv_imp.sh
-. import/app_imp.sh
+. inports/info_imp.sh
+. imports/sist_imp.sh
+. imports/serv_imp.sh
+. imports/app_imp.sh
 ########################################################################
 ########################################################################
 # VARIABLES USADAS EN ESTE SCRIPT
@@ -43,7 +43,7 @@ case $opcion in
 
     # Performs the function with the name of the variable passed
     0) clear; exit;;
-    1) clear; config_info,sh; sleep 6; upgrade.sh; pikey.sh; rootkey.sh; timezone.sh; timezone.sh; expandir.sh; lared.sh; ready;;
+    1) clear; config_info.sh; sleep 6; upgrade.sh; pikey.sh; rootkey.sh; timezone.sh; expandir.sh; lared.sh; ready;;
     #1) clear; config_info,sh; sleep 6; ready;;
     #2) upgrade.sh; curl.sh; git.sh; node.sh; chromium.sh; docker.sh; vscode.sh; ready;;
     2) sudo chmod +x apps.sh ; source apps.sh;;
